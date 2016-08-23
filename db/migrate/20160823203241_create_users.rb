6da590c5105eb65d73f7e4685b7266746a92a6b1
tttra <tothutra@gmail.com>
2016-08-23 16:45:34 -0400

    add Devise to User

--- db/migrate/20160823203241_create_users.rb
+++ db/migrate/20160823203241_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
       t.integer :role, default: 0
       t.timestamps null: false
     end
