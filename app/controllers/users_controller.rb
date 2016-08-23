1cca0a577a1baf38f63aa31c9b6f825b738f2949
tttra <tothutra@gmail.com>
2016-08-23 17:45:42 -0400

    Add User show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
+
+  def show
+    
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
