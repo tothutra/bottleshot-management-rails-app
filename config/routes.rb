f75539cbdcd5f9e87125d774d7b654cfe79a761b
tttra <tothutra@gmail.com>
2016-08-23 11:51:00 -0400

    implement header html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'site/home'
+
+  root 'site#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ note.md
@@ -0,0 +1,4 @@
+Step 1: Apply css asset 
+  Create SiteController and stub out Homepage
+
+Step 2: 
