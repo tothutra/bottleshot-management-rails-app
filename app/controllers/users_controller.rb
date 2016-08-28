8b52f0bb578db327e8b7009825f88346751cd187
tttra <tothutra@gmail.com>
2016-08-28 13:37:55 -0400

    add company column to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,25 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
 
   def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user)
   end
 
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,19 @@
+class UserPolicy < ApplicationPolicy
+  
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record == user
+  end
+
+  def destroy?
+    user.admin? 
+  end
+
+  def update?
+    user.admin? || record == user
+  end
+
+end
