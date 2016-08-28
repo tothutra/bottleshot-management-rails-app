bb07a6efed737da130f6160715ad19e9494286f4
tttra <tothutra@gmail.com>
2016-08-28 12:13:21 -0400

    add CRUD to LabelFinish

--- app/controllers/admin/glass_colors_controller.rb
+++ app/controllers/admin/glass_colors_controller.rb
@@ -1,2 +1,47 @@
-class GlassColorsController < ApplicationController
+class Admin::GlassColorsController < ApplicationController
+
+  before_action :set_glass_color, except: [:index, :create]
+
+  def index
+    @glass_colors = GlassColor.all
+    @glass_color = GlassColor.new
+  end
+
+  def show
+    @bottleshots = @glass_color.bottleshots    
+  end
+
+  def create
+    @glass_color = GlassColor.new(glass_color_params)
+    if @glass_color.save
+      redirect_to admin_glass_colors_path
+    else
+      render 'admin/glass_colors/index'
+    end    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @glass_color.update(glass_color_params)
+    redirect_to admin_glass_colors_path    
+  end
+
+  def destroy
+    @glass_color.destroy
+    redirect_to admin_glass_colors_path    
+  end
+
+  private
+
+  def set_glass_color
+    @glass_color = GlassColor.find(params[:id])
+  end
+
+  def glass_color_params
+    params.require(:glass_color).permit(:name, :description)
+  end
+
 end
--- app/controllers/admin/label_finishes_controller.rb
+++ app/controllers/admin/label_finishes_controller.rb
@@ -1,2 +1,47 @@
-class LabelFinishesController < ApplicationController
+class Admin::LabelFinishesController < ApplicationController
+
+  before_action :set_label_finish, except: [:index, :create]
+
+  def index
+    @label_finishes = LabelFinish.all
+    @label_finish = LabelFinish.new
+  end
+
+  def show
+    @bottleshots = @label_finish.bottleshots    
+  end
+
+  def create
+    @label_finish = LabelFinish.new(label_finish_params)
+    if @label_finish.save
+      redirect_to admin_label_finishes_path
+    else
+      render 'admin/label_finishes/index'
+    end    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @label_finish.update(label_finish_params)
+    redirect_to admin_label_finishes_path    
+  end
+
+  def destroy
+    @label_finish.destroy
+    redirect_to admin_label_finishes_path    
+  end
+
+  private
+
+  def set_label_finish
+    @label_finish = LabelFinish.find(params[:id])
+  end
+
+  def label_finish_params
+    params.require(:label_finish).permit(:name, :description)
+  end
+
 end
