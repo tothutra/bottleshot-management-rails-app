14cbff6a43fb7cdf2ba60b829eb30d998b5cff7c
tttra <tothutra@gmail.com>
2016-08-28 15:16:18 -0400

    Add UserPolicy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-
+  #before_action :authenticate_user!
   before_action :set_user, only: [:index, :show, :edit, :update]
 
   def show
-    
+    authorize @user
   end
 
   def edit
-    
   end
 
   def update
+    authorize @user
     @user.update(user_params)
     redirect_to user_path(@user)
   end
