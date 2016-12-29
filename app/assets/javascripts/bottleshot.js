9e2d06e963c994e0a10fc0d42d14d9b86c03e6fc
tttra <tothutra@gmail.com>
2016-12-29 13:24:11 -0500

    refactor js model

--- app/assets/javascripts/bottleshot.js
+++ app/assets/javascripts/bottleshot.js
@@ -1,15 +1,8 @@
 function Bottleshot(attributes){
   this.varietal = attributes.varietal
   this.vintage = attributes.vintage
-  this.bottle_shape = attributes.bottle_shape
-  this.glass_color = attributes.glass_color
-  this.wine_color = attributes.wine_color
-  this.capsule = attributes.capsule
-  this.paperstock = attributes.paperstock
-  this.label_finish = attributes.label_finish
-  this.screenprinted = attributes.screenprinted
-  this.add_ons = attributes.add_ons
   this.fullname = attributes.vintage + " " + attributes.varietal
+  this.screenprinted = attributes.screenprinted
   this.is_screenprinted = function() {
     if (this.screenprinted == true) {
       return "Yes"
@@ -17,11 +10,20 @@ function Bottleshot(attributes){
       return "No"
     }
   }
+  /*this.bottle_shape = attributes.bottle_shape
+  this.glass_color = attributes.glass_color
+  this.wine_color = attributes.wine_color
+  this.capsule = attributes.capsule
+  this.paperstock = attributes.paperstock
+  this.label_finish = attributes.label_finish
+  this.add_ons = attributes.add_ons
+  
+  
   this.all_add_ons = function() {
     var add_ons = []
     this.add_ons.forEach(function(add_on) {
       add_ons.push(add_on["name"])
     })
     return add_ons.join(", ")
-  }
+  }*/
 }
